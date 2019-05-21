ce94493185a85d7a63c18f8db66f3841ea2ebb32
guanyichen <trcgabriella@163.com>
2019-05-21 20:53:14 +0800

    Bean管理，属性注入的注解方式

--- src/main/java/com/imooc/ioc_annotation/SpringDemo6.java
+++ src/main/java/com/imooc/ioc_annotation/SpringDemo6.java
@@ -12,4 +12,17 @@ public class SpringDemo6 {
         String s= userService.sayHello("张三");
         System.out.println(s);
     }
+
+    @Test
+    public void demo2(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.eat();
+    }
+    @Test
+    public void demo3(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.save();
+    }
 }
--- src/main/java/com/imooc/ioc_annotation/UserService.java
+++ src/main/java/com/imooc/ioc_annotation/UserService.java
@@ -1,7 +1,13 @@
 package com.imooc.ioc_annotation;
 
+import com.imooc.ioc.demo4.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Spring 的Bean管理：注解方式
@@ -10,7 +16,25 @@ import org.springframework.stereotype.Service;
 //@Component("userService")
     @Service("userService")
 public class UserService {
+    //普通类型注释
+    @Value("Pizza")
+    private String something;
+    //对象类型
+//    @Autowired
+//    @Qualifier("userDao111")
+    @Resource(name="userDao111")
+    private UserDao userDao;
+
     public String sayHello(String name){
         return "Hello "+name;
+    }
+
+    public void eat(){
+        System.out.println("eat "+ something);
+    }
+
+    public void save(){
+        System.out.println("Service中保存用户。。。");
+        userDao.save();
     }
 }
