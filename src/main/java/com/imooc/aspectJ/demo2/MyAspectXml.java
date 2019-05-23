5d5f28b743c18786fc4d144a3ae4a33e327d775b
guanyichen <trcgabriella@163.com>
2019-05-23 12:35:20 +0800

    基于AspectJ的XML方式 前置通知+参数返回值JoinPoint

--- src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
+++ src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
@@ -1,11 +1,13 @@
 package com.imooc.aspectJ.demo2;
 
+import org.aspectj.lang.JoinPoint;
+
 public class MyAspectXml {
 
 
     //前置通知
-    public void before(){
-        System.out.println("XML方式的前置通知===========");
+    public void before(JoinPoint joinPoint){
+        System.out.println("XML方式的前置通知==========="+joinPoint);
     }
 
 
