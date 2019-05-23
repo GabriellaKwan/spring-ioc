741a1c9544d75b59273e8eb13e90a913a973dd45
guanyichen <trcgabriella@163.com>
2019-05-23 12:29:15 +0800

    基于AspectJ的XML配置方式的AOP，以前置为例

--- src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
+++ src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
@@ -1,4 +1,12 @@
 package com.imooc.aspectJ.demo2;
 
 public class MyAspectXml {
+
+
+    //前置通知
+    public void before(){
+        System.out.println("XML方式的前置通知===========");
+    }
+
+
 }
--- src/main/java/com/imooc/aspectJ/demo2/SpringDemo10.java
+++ src/main/java/com/imooc/aspectJ/demo2/SpringDemo10.java
@@ -1,4 +1,26 @@
 package com.imooc.aspectJ.demo2;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:applicationContext_aspectJ2.xml")
 public class SpringDemo10 {
+
+    @Resource(name = "customerDao")
+    private CustomerDao customerDao;
+
+    @Test
+    public void demo1(){
+        customerDao.save();;
+        customerDao.delete();
+        customerDao.update();
+        customerDao.findOne();
+        customerDao.findAll();
+    }
+
 }
