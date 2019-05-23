54f9c378289c1e01dc5c4bcea85a5da9b9461985
guanyichen <trcgabriella@163.com>
2019-05-23 12:13:11 +0800

    基于AspectJ的XML方式AOP开发，环境搭建

---
+++ src/main/java/com/imooc/aspectJ/demo2/CustomerDaoImpl.java
@@ -0,0 +1,31 @@
+package com.imooc.aspectJ.demo2;
+
+public class CustomerDaoImpl implements CustomerDao{
+
+
+    @Override
+    public void save() {
+        System.out.println("保存客户...");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("修改客户...");
+
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("删除客户...");
+    }
+
+    @Override
+    public void findOne() {
+        System.out.println("查询一个客户...");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("查询多个客户...");
+    }
+}
