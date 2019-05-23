6c634badbc855aa08e516be52d7d0cb864ccbad3
guanyichen <trcgabriella@163.com>
2019-05-23 11:32:15 +0800

    AspectJ环绕通知，ProceedingJointPoint

--- src/main/java/com/imooc/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/imooc/aspectJ/demo1/MyAspectAnno.java
@@ -1,7 +1,10 @@
 package com.imooc.aspectJ.demo1;
 
+import javafx.beans.binding.ObjectExpression;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -19,5 +22,13 @@ public class MyAspectAnno {
     @AfterReturning(value = "execution(* com.imooc.aspectJ.demo1.ProductDao.update(..))",returning = "result")
     public void  afterReturning(Object result){
         System.out.println("后置通知=================="+result);
+    }
+
+    @Around(value = "execution(* com.imooc.aspectJ.demo1.ProductDao.delete(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("环绕前通知==============");
+        Object obj = joinPoint.proceed(); //执行目标方法
+        System.out.println("环绕后通知==============");
+        return obj;
     }
 }
