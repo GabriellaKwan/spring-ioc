30cc5007af6a47ee4366e6363ebefeed28aff9a7
guanyichen <trcgabriella@163.com>
2019-05-23 12:38:53 +0800

    基于AspectJ的XML方式 后置通知

--- src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
+++ src/main/java/com/imooc/aspectJ/demo2/MyAspectXml.java
@@ -9,6 +9,10 @@ public class MyAspectXml {
     public void before(JoinPoint joinPoint){
         System.out.println("XML方式的前置通知==========="+joinPoint);
     }
+    // 后置通知
+    public void afterReturning(){
+        System.out.println("XML方式的后置通知==============");
+    }
 
 
 }
