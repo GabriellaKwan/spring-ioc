535a957313397b3078ad54c81a54288a88bb9000
guanyichen <trcgabriella@163.com>
2019-05-28 12:08:11 +0800

    JDBC Template 增删改 update和batchUpdate

--- src/main/java/com/imooc/jdbc_template/jdbcTemplateTest.java
+++ src/main/java/com/imooc/jdbc_template/jdbcTemplateTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 //注解方法
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,5 +31,37 @@ public class jdbcTemplateTest {
     public void testExecute(){
         jdbcTemplate.execute("create table user2(id int, name varchar(20))");
     }
+
+    @Test
+    public void testUpdate(){
+        String sql = "insert into student(name,sex) values(?,?)";
+        jdbcTemplate.update(sql,new Object[]{"张飞","男"});
+    }
+
+    @Test
+    public void testUpdate2(){
+        String sql = "update student set sex=? where id=?";
+        jdbcTemplate.update(sql,"女",1);
+    }
+
+    //批量处理
+    @Test
+    public void testBatchUpdate(){
+        String[] sqls={
+                "insert into student(name,sex) values('关羽','女')",
+                "insert into student(name,sex) values('刘备','男')",
+                "update student set sex='女' where id=1"
+        };
+        jdbcTemplate.batchUpdate(sqls);
+    }
+    @Test
+    public void testBatchUpdate2(){
+        String sql = "insert into selection(student,course) values(?,?)";
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{3,1001});
+        list.add(new Object[]{4,1003});
+        jdbcTemplate.batchUpdate(sql,list);
+    }
+
 
 }
